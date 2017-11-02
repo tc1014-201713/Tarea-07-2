d57fd876a975c36e9981135dd11e898351cd05af
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-11-02 12:10:11 -0600

    Update listas2.py

--- listas2.py
+++ listas2.py
@@ -20,6 +20,40 @@ def crearListaMayores(listaOriginal):
 
 def intercambiarPares(listaOriginal):    
     
+
+    
+def definirNumerosMayoresAlPromedio(listaOriginal):
+  lenOriginal=len(listaOriginal)
+  promedio= sum(listaOriginal) / lenOriginal
+  listaFinal=[]
+  contador=0
+  for j in listaOriginal:
+    if j=>promedio:
+      contador+=1
+      listaFinal.append(j)
+  return(contador, promedio,listaFinal)
+      
+def cambiarPosicionMinMax(listaOriginal):
+  valormin=min(listaOriginal)
+  valormax=max(listaOriginal)
+  posicionmin=listaOriginal.index(valormin)
+  print(posicionmin)
+  posicionmax=listaOriginal.index(valormax)
+  print(posicionmax)
+  listaIntercambiada=listaOriginal[:]
+  listaIntercambiada.remove(valormax)
+  listaIntercambiada.insert(posicionmin,valormax)
+  listaIntercambiada.remove(valormin)
+  listaIntercambiada.insert(posicionmax,valormin)
+  return(listaIntercambiada)
+  
+  
+  
+    
+def calcularMediaYDesviacionEstandar(listaOriginal):
+  lenOriginal=len(listaOriginal)
+  media=sum(listaOriginal) / lenOriginal
+    
     
 def main():
 
