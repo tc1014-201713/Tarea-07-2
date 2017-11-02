5f56d3735cc0d5795fa55f79d7093852296425ce
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-11-02 11:48:29 -0600

    Update listas2.py

--- listas2.py
+++ listas2.py
@@ -3,3 +3,25 @@
 
 
 def crearListaConPares(listaOriginal):
+  listaPares=[]
+  for i in listaOriginal:
+    if i%2==0:
+      listaPares.append(i)
+  return(listaPares)
+      
+def crearListaMayores(listaOriginal):
+  listaMayores=[]
+  anterior=0
+  for i in listaOriginal:
+    if i > anterior:
+      listaMayores.append(i)
+    anterior=i
+  return(listaMayores)
+
+def intercambiarPares(listaOriginal):    
+    
+    
+def main():
+
+main()
+    
