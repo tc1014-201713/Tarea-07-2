46beb6659e0125889eb4f19dc8761b8f61862580
CINCONUMBERCERO <cinconumbercero@hotmail.com>
2017-11-02 22:11:15 -0600

    comentarios pendientes

--- listas2.py
+++ listas2.py
@@ -1,12 +1,13 @@
 #Encoding: UTF-8
 #Autor Aaron Villanueva
 
-listaA=[1,2,3,4,5]
+listaA=[1,2,3,4,5,6]
 listaB=[5,7,1,6,9,12,4]
 listaC=[2,6,3,4]
 listaD=[5]
 listaE=[55,44,77]
-listaDeListas=[listaA,listaB,listaC,listaD,listaE]
+listaF=[]
+listaDeListas=[listaA,listaB,listaC,listaD,listaE,listaF]
 
 def crearListaConPares(listaOriginal):
     listaPares=[]
@@ -27,39 +28,44 @@ def crearListaMayores(listaOriginal):
 def intercambiarPares(listaOriginal):    
     listaNueva=[]
     contador=0
-    for j in listaOriginal:
-        posicion=listaOriginal.index(j)
-    if posicion==len(listaOriginal):
-        listaNueva.append(j)
-    elif posicion%2==1:
-        listaNueva.insert(contador-1,j)
-    else:
-        listaNueva.insert(contador,j)
-    contador+=1
+    if len(listaOriginal)>1:
+        for j in listaOriginal:
+            posicion=listaOriginal.index(j)
+        if posicion==len(listaOriginal):
+            listaNueva.append(j)
+        elif posicion%2==1:
+            listaNueva.insert(contador-1,j)
+        else:
+            listaNueva.insert(contador,j)
+        contador+=1
     return(listaNueva)
 
 
 def definirNumerosMayoresAlPromedio(listaOriginal):
     lenOriginal=len(listaOriginal)
-    promedio= sum(listaOriginal) / lenOriginal
-    listaFinal=[]
-    contador=0
-    for j in listaOriginal:
-        if j >= promedio:
-            contador+=1
-            listaFinal.append(j)
-    return(contador, promedio,listaFinal)
+    if lenOriginal>0:
+        promedio= sum(listaOriginal) / lenOriginal
+        listaFinal=[]
+        contador=0
+        for j in listaOriginal:
+            if j >= promedio:
+                contador+=1
+                listaFinal.append(j)
+        return(contador, promedio,listaFinal)
+    else:
+        return(0,0,listaOriginal)
 
 def cambiarPosicionMinMax(listaOriginal):
-    valormin=min(listaOriginal)
-    valormax=max(listaOriginal)
-    posicionmin=listaOriginal.index(valormin)
-    posicionmax=listaOriginal.index(valormax)
-    listaIntercambiada=listaOriginal[:]
-    listaIntercambiada.remove(valormax)
-    listaIntercambiada.insert(posicionmin,valormax)
-    listaIntercambiada.remove(valormin)
-    listaIntercambiada.insert(posicionmax,valormin)
+    listaIntercambiada = listaOriginal[:]
+    if len(listaOriginal)>1:
+        valormin=min(listaOriginal)
+        valormax=max(listaOriginal)
+        posicionmin=listaOriginal.index(valormin)
+        posicionmax=listaOriginal.index(valormax)
+        listaIntercambiada.remove(valormax)
+        listaIntercambiada.insert(posicionmin,valormax)
+        listaIntercambiada.remove(valormin)
+        listaIntercambiada.insert(posicionmax,valormin)
     return(listaIntercambiada)
   
   
@@ -67,16 +73,12 @@ def cambiarPosicionMinMax(listaOriginal):
 
 def calcularMediaYDesviacionEstandar(listaOriginal):
     lenOriginal=len(listaOriginal)
-    print("la len es",lenOriginal)
-    media=sum(listaOriginal) / lenOriginal
-    print("la media es",media)
+    media=0
     if lenOriginal>1:
+        media = sum(listaOriginal) / lenOriginal
         sumatoria=0
         for i in listaOriginal:
             sumatoria+=(i-media)**2
-            print(sumatoria)
-
-        sumatoria=sumatoria**2
         adentro=sumatoria/ (lenOriginal-1)
         desviacion=adentro**.5
     else:
@@ -89,26 +91,27 @@ def main():
     print("Ejercicio 1. Crear lista con solo números pares")
     for k in listaDeListas:
         print("Si recibe",k,",regresa",crearListaConPares(k))
-
+    print("")
     print("Ejercicio 2. Crear lista con los valores mayores al anterior")
     for k in listaDeListas:
         print("Si recibe", k, ",regresa", crearListaMayores(k))
-
+    print("")
     print("Ejercicio 3. Intercambiar pareja de datos")
     for k in listaDeListas:
         print("Si recibe", k, ",regresa", intercambiarPares(k))
-
+    print("")
     print("Ejercicio 4. Intercambiar mayor con menor")
     for k in listaDeListas:
         print("Si recibe", k, ",regresa", cambiarPosicionMinMax(k))
-
+    print("")
     print("Ejercicio 5. Encontrar valores mayores al promedio")
     for k in listaDeListas:
-        cont, prome, listF= definirNumerosMayoresAlPromedio(k)
-        print("Si recibe", k, ",regresa %d. El promedio es %.2f y hay %d valores iguales o mayores que %.2f" %(cont,prome,cont,prome))
-
+        cont, prome, listFinal= definirNumerosMayoresAlPromedio(k)
+        print("Si recibe", k, ",regresa %d. El promedio es %.2f y hay %d valores iguales o mayores que %.2f" %(cont,prome,cont,prome), listFinal )
+    print("")
     print("Ejercicio 6. Encontrar la desviación estandar")
-    for k in listaDeListas:
-        print("Si recibe",k,"regresa",calcularMediaYDesviacionEstandar(k))
+    for j in listaDeListas:
+        final=calcularMediaYDesviacionEstandar(j)
+        print("Si recibe",j,"regresa",final)
 
 main()
