4fc45647341b63c204109799f07d366444cfbb54
CINCONUMBERCERO <cinconumbercero@hotmail.com>
2017-11-05 22:11:15 -0600

    Terminado

--- listas2.py
+++ listas2.py
@@ -1,6 +1,7 @@
 #Encoding: UTF-8
 #Autor Aaron Villanueva
 
+#listas a utilizar en las pruebas de cada función
 listaA=[1,2,3,4,5,6]
 listaB=[5,7,1,6,9,12,4]
 listaC=[2,6,3,4]
@@ -9,6 +10,7 @@ listaE=[55,44,77]
 listaF=[]
 listaDeListas=[listaA,listaB,listaC,listaD,listaE,listaF]
 
+#Esta función crea una nueva lista con los valores pares de una lista provista
 def crearListaConPares(listaOriginal):
     listaPares=[]
     for i in listaOriginal:
@@ -16,6 +18,7 @@ def crearListaConPares(listaOriginal):
             listaPares.append(i)
     return(listaPares)
 
+#Esta función crea una nueva lista con los valores que sean mayores al anterior
 def crearListaMayores(listaOriginal):
     listaMayores=[]
     anterior=0
@@ -25,22 +28,24 @@ def crearListaMayores(listaOriginal):
         anterior=i
     return(listaMayores)
 
+#Esta función intercambia la posición de cada par de datos de una lista provista. Si la lista tiene un
+#numero impar de datos, el último no se mueve.
 def intercambiarPares(listaOriginal):    
     listaNueva=[]
     contador=0
     if len(listaOriginal)>1:
         for j in listaOriginal:
             posicion=listaOriginal.index(j)
-        if posicion==len(listaOriginal):
-            listaNueva.append(j)
-        elif posicion%2==1:
-            listaNueva.insert(contador-1,j)
-        else:
-            listaNueva.insert(contador,j)
-        contador+=1
+            if posicion==len(listaOriginal):
+                listaNueva.append(j)
+            elif posicion%2==1:
+                listaNueva.insert(contador-1,j)
+            else:
+                listaNueva.insert(contador,j)
+            contador+=1
     return(listaNueva)
 
-
+#Esta función calcula el promedio de un lista y regresa una lista nueva con los valores que superen el promedio.
 def definirNumerosMayoresAlPromedio(listaOriginal):
     lenOriginal=len(listaOriginal)
     if lenOriginal>0:
@@ -55,6 +60,7 @@ def definirNumerosMayoresAlPromedio(listaOriginal):
     else:
         return(0,0,listaOriginal)
 
+#Esta función intercambia la posición del valor mínimo y máximo de una lista provista
 def cambiarPosicionMinMax(listaOriginal):
     listaIntercambiada = listaOriginal[:]
     if len(listaOriginal)>1:
@@ -68,9 +74,7 @@ def cambiarPosicionMinMax(listaOriginal):
         listaIntercambiada.insert(posicionmax,valormin)
     return(listaIntercambiada)
   
-  
-  
-
+#Esta función calcula el promedio y la desviación estandar de una lista.
 def calcularMediaYDesviacionEstandar(listaOriginal):
     lenOriginal=len(listaOriginal)
     media=0
@@ -87,7 +91,6 @@ def calcularMediaYDesviacionEstandar(listaOriginal):
 
 
 def main():
-
     print("Ejercicio 1. Crear lista con solo números pares")
     for k in listaDeListas:
         print("Si recibe",k,",regresa",crearListaConPares(k))
